fa518f991bb4cfae89033e88e12c6d6ee9a94f4f
PakkuDon <pakku.pd@gmail.com>
2016-12-01 15:56:02 +1100

    Add placeholder for ticketing routes

---
+++ routes/ticket.js
@@ -0,0 +1,10 @@
+// Routes for registration / ticketing part
+var express = require('express');
+
+var router = express.Router();
+
+router.post('/', (req, res) => {
+  res.sendStatus(501);
+});
+
+module.exports = router;
