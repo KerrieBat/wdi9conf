d79a95e1f49407c970c6f3e792d62fcc5c4cf6ca
Kerrie Batrouney <batrouney@gmail.com>
2016-12-02 12:03:20 +1100

    add possible images

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 // Routes
+app.get('/', function(req, res){
+  res.render('index')
+})
 
 // Start server
 app.listen(app.get('port'), () => {
