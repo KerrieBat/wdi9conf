ac018ec6523d9ef3ab9989feda9214e28d7552b4
Kerrie Batrouney <batrouney@gmail.com>
2016-12-05 09:02:53 +1100

    weekend work

--- client/scripts/display.js
+++ client/scripts/display.js
@@ -1,10 +1,9 @@
-// for parallex feature
-$(document).ready(function(){
-     $('.parallax').parallax();
-   });
-
 
 //for header
 $(document).ready(function(){
     $('.slider').slider({full_width: true});
+    $('.slider').slider({transition: 4500});
+    $('.collapsible').collapsible();
+
   });
+  console.log('here are the queens of GA');
