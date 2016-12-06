755a14a98b6d19750a49502d32c4052bdb016e78
Elizabeth Jones <elizabeth.anna.jones@gmail.com>
2016-12-06 17:36:35 +1100

    css updates

--- client/scripts/display.js
+++ client/scripts/display.js
@@ -1,13 +1,9 @@
 
 //for header
 $(document).ready(function(){
-      $(".button-collapse").sideNav();
     $('.slider').slider({full_width: true});
     $('.slider').slider({transition: 4500});
     $('.collapsible').collapsible();
-
-
-
   });
   console.log('here are the queens of GA');
 
