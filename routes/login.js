1ebbfde3589ad24940522775649a4dd1c6c8a346
Catherine Gracey <catherine.gracey@gmail.com>
2016-12-07 15:12:29 +1100

    Registered users can log in

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,31 @@
 var express = require('express');
 
 var router = express.Router();
+var db = require('../db.js');
+var password = require('./password');
 
 router.post('/', (req, res) => {
-  res.render('loggedin');
+  var registrant = {
+    email: req.body.email
+  }
+  db('users').where({
+    email: registrant.email
+  }).
+  then(function(user){
+    if (user.length > 0){
+      if (password.isPasswordValid(req.body.password, user[0].password)){
+        //The user has already registered with this password, so just log them in.
+        res.render('loggedin', user[0]);
+      } else {
+        console.log(user[0].password, registrant.password)
+        registrant.login_error = "Please check your password for typos and try again."
+        res.render('index', registrant);
+      }
+    } else {
+      registrant.login_error = "Please check your email address for typos and try again."
+      res.render('index', registrant);
+    }
+  });
 });
 
 module.exports = router;
