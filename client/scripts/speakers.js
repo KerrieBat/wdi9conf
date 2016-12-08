9005b653cb99f14c0c23e718c3a18af183de3ead
Kerrie Batrouney <batrouney@gmail.com>
2016-12-09 09:24:56 +1100

    speaker bios

--- client/scripts/speakers.js
+++ client/scripts/speakers.js
@@ -1,8 +1,7 @@
 
+
 var harry = '<p>Harry Aydin</p>'
 var victor = '<p>Bret Victor</p>'
-
-var martin = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit'
 
   if ($(window).width() > 700) {
     $('#lone-blurb').html(victor).css({
@@ -42,7 +41,7 @@ var martin = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do e
 
 $( "#lone-speaker" ).hover(
   function() {
-    $('#lone-blurb').html(larry).css({
+    $('#lone-blurb').html(harry).css({
       'visibility': 'visible',
       'opacity': 1,
       'color': 'orange'
