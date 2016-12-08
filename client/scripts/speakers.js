89ff94384687502d31f20c807d38f7c39d208443
Micah Boyd <micahhope@yahoo.com>
2016-12-08 21:38:17 +1100

    fixed Harry burb jquery + speaker font sizing on small screen

--- client/scripts/speakers.js
+++ client/scripts/speakers.js
@@ -1,15 +1,13 @@
 
-<<<<<<< HEAD
 var harry = '<p>Harry Aydin</p>'
 var victor = '<p>Bret Victor</p>'
-=======
-var larry = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit'
->>>>>>> 277bd3438371a296c93c334334e8cd971d5a36ae
 
-var martin = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit'
+function responsiveJS() {
 
-<<<<<<< HEAD
   if ($(window).width() > 700) {
+
+    $( '.speakers-page p' ).removeClass('flow-text');
+
     $('#lone-blurb').html(victor).css({
       'visibility': 'hidden',
       'opacity': 0
@@ -35,7 +33,7 @@ var martin = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do e
           'visibility': 'visible',
           'opacity': 1,
           'color': 'orange'
-          }).html(martin);
+        }).html(victor);
       }, function() {
         $('#lone-blurb').css({
           'visibility': 'hidden',
@@ -43,33 +41,22 @@ var martin = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do e
           });
     })
   } else {
-    console.log('< 800');
-=======
-$( "#lone-speaker" ).hover(
-  function() {
-    $('#lone-blurb').html(larry).css({
-      'visibility': 'visible',
-      'opacity': 1,
-      'color': 'orange'
-      });
-  }, function() {
-    $('#lone-blurb').css({
-      'visibility': 'hidden',
-      'opacity': 0,
-      });
-})
->>>>>>> 277bd3438371a296c93c334334e8cd971d5a36ae
 
-$( "#middle-speaker" ).hover(
-  function() {
-    $('#lone-blurb').html(martin).css({
+    $( '#lone-blurb' ).html(victor).css({
       'visibility': 'visible',
       'opacity': 1,
-      'color': 'orange'
       });
-  }, function() {
-    $('#lone-blurb').css({
-      'visibility': 'hidden',
-      'opacity': 0,
-      });
+
+      if ($(window).width() > 400) {
+        $( '.speakers-page p' ).addClass('flow-text');
+        $( '#lone-blurb p' ).addClass('flow-text');
+      }
+      $( "#middle-speaker" ).off();
+      $( "#lone-speaker" ).off();
+  }
+};
+
+responsiveJS();
+$( window ).scroll(function() {
+  responsiveJS();
 })
