eaf6f9ecbd3f762f9a8c53371f190ba289c15044
Micah Boyd <micahhope@yahoo.com>
2016-12-08 16:19:06 +1100

    fixed login scroll, moved scroll logic to display.js

--- client/scripts/display.js
+++ client/scripts/display.js
@@ -35,4 +35,20 @@ $(document).ready(function(){
     window.addEventListener("hashchange", shiftWindow);
   });
 
+  // navigation scrollBy
+
+  $("ul").click('a', function (event){
+    var navItemID = event.target.id
+    // removing '-nav' from ID to scroll to appropriate section
+    var section = navItemID.split('-')[0]
+    if (section === 'login') {
+      section = 'register'
+    }
+    var scrollTo = $('#' + section).offset().top;
+    $('html, body').animate({
+      scrollTop: scrollTo
+    }, 500);
+  });
+
+
   console.log('here are the queens of GA');
